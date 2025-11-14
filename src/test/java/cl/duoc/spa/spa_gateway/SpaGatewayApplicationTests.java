de864077b4f69d317e93077b923fc9d3ba85d6c3
PalvarezLara <pame.alvarez@duocuc.cl>
2025-11-13 22:40:35 -0300

    Initial Spring Boot project commit

---
+++ src/test/java/cl/duoc/spa/spa_gateway/SpaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.spa.spa_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
